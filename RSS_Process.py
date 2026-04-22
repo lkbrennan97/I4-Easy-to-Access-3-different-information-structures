994272bcdfb9e1038632887863f74b534393df29
Lincoln Brennan <lbrenna@uw.edu>
2026-04-22 11:54:00 -0700

    Update and rename RSS Process to RSS_Process.py

---
+++ RSS_Process.py
@@ -0,0 +1,2 @@
+# RSS:  view-source:https://opiumhum.blogspot.com/
+# <link rel="alternate" type="application/rss+xml" title="_ O _ P _ I _ U _ M __ H _ U _ M _ - RSS" href="https://opiumhum.blogspot.com/feeds/posts/default?alt=rss" />
